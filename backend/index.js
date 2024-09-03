e085d73145b8d6b9d182c2ddb4827dc5f80cf09a
Ali Samir <ali.samir@taager.com>
2024-09-03 21:59:34 +0300

    Project Compeleted

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,10 @@ app.use(cookieParser()); // for parsing cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
   });
 }
 
